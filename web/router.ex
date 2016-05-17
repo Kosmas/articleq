28c6017ffc29389212b208501410b0f127aa31ed
Kosmas Chatzimichalis <kosmas@mach7x.com>
2016-05-17 23:26:40 +0200

    add guardian's VerifyHeader and LoadResource plugs to api scope

--- web/router.ex
+++ web/router.ex
@@ -11,6 +11,8 @@ defmodule Articleq.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug Guardian.Plug.VerifyHeader
+    plug Guardian.Plug.LoadResource
   end
 
   scope "/", Articleq do
